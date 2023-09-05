922013c6d18b8515700732d4e851e779fb5c48ee
MohamadKhavaran <133672393+MohamadKhavaran@users.noreply.github.com>
2023-09-05 12:47:10 +0430

    Design Main Page
    !- Background Image Will Add After
    - Add Some Labels And Add it's Events
    🪄

--- CoffeeShop/Source/WindowsFormsApp1/WindowsFormsApp1/UserInterFaces/FormMain.Designer.cs
+++ CoffeeShop/Source/WindowsFormsApp1/WindowsFormsApp1/UserInterFaces/FormMain.Designer.cs
@@ -31,44 +31,66 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.Menu = new System.Windows.Forms.Label();
             this.About = new System.Windows.Forms.Label();
+            this.Login_as_admin = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // Menu
             // 
             this.Menu.AutoSize = true;
             this.Menu.BackColor = System.Drawing.SystemColors.Desktop;
+            this.Menu.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Menu.Font = new System.Drawing.Font("Cooper Black", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Menu.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.Menu.Image = global::WindowsFormsApp1.Properties.Resources._8610577;
             this.Menu.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.Menu.Location = new System.Drawing.Point(921, 36);
+            this.Menu.Location = new System.Drawing.Point(918, 36);
             this.Menu.Name = "Menu";
             this.Menu.Size = new System.Drawing.Size(360, 91);
             this.Menu.TabIndex = 0;
             this.Menu.Text = "Menu     ";
-            this.Menu.Click += new System.EventHandler(this.label1_Click);
+            this.Menu.MouseEnter += new System.EventHandler(this.Menu_MouseEnter);
+            this.Menu.MouseLeave += new System.EventHandler(this.Menu_MouseLeave);
             // 
             // About
             // 
             this.About.AutoSize = true;
             this.About.BackColor = System.Drawing.SystemColors.Desktop;
+            this.About.Cursor = System.Windows.Forms.Cursors.Hand;
             this.About.Font = new System.Drawing.Font("Cooper Black", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.About.ForeColor = System.Drawing.SystemColors.ButtonFace;
             this.About.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.About.Location = new System.Drawing.Point(862, 150);
+            this.About.Location = new System.Drawing.Point(859, 200);
             this.About.Name = "About";
             this.About.Size = new System.Drawing.Size(419, 91);
             this.About.TabIndex = 1;
             this.About.Text = "About Us";
-            this.About.Click += new System.EventHandler(this.About_Click);
+            this.About.MouseEnter += new System.EventHandler(this.About_MouseEnter);
+            this.About.MouseLeave += new System.EventHandler(this.About_MouseLeave);
+            // 
+            // Login_as_admin
+            // 
+            this.Login_as_admin.AutoSize = true;
+            this.Login_as_admin.BackColor = System.Drawing.SystemColors.Desktop;
+            this.Login_as_admin.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Login_as_admin.Font = new System.Drawing.Font("Cooper Black", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login_as_admin.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.Login_as_admin.ImageAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.Login_as_admin.Location = new System.Drawing.Point(610, 366);
+            this.Login_as_admin.Name = "Login_as_admin";
+            this.Login_as_admin.Size = new System.Drawing.Size(668, 91);
+            this.Login_as_admin.TabIndex = 2;
+            this.Login_as_admin.Text = "Login as Admin";
+            this.Login_as_admin.MouseEnter += new System.EventHandler(this.Login_as_admin_MouseEnter);
+            this.Login_as_admin.MouseLeave += new System.EventHandler(this.Login_as_admin_MouseLeave);
             // 
             // FormMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackgroundImage = global::WindowsFormsApp1.Properties.Resources.coffee_rorozoa_3_04;
+            this.BackColor = System.Drawing.Color.IndianRed;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1323, 729);
+            this.ClientSize = new System.Drawing.Size(1334, 718);
+            this.Controls.Add(this.Login_as_admin);
             this.Controls.Add(this.About);
             this.Controls.Add(this.Menu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
@@ -88,6 +110,7 @@
 
         private System.Windows.Forms.Label Menu;
         private System.Windows.Forms.Label About;
+        private System.Windows.Forms.Label Login_as_admin;
     }
 }
 
---
+++ CoffeeShop/Source/WindowsFormsApp1/WindowsFormsApp1/UserInterFaces/FormMain.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+namespace WindowsFormsApp1
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+            Menu.BackColor = Color.Transparent;
+            About.BackColor = Color.Transparent;    
+            Login_as_admin.BackColor = Color.Transparent;
+
+        }
+
+        private void Menu_MouseEnter(object sender, EventArgs e)
+        {
+            Menu.ForeColor = Color.Yellow; 
+        }
+
+        private void Menu_MouseLeave(object sender, EventArgs e)
+        {
+            Menu.ForeColor= Color.White;
+        }
+
+        private void About_MouseEnter(object sender, EventArgs e)
+        {
+            About.ForeColor = Color.Yellow;
+        }
+
+        private void About_MouseLeave(object sender, EventArgs e)
+        {
+            About.ForeColor = Color.White;
+
+        }
+
+        private void Login_as_admin_MouseEnter(object sender, EventArgs e)
+        {
+            Login_as_admin.ForeColor = Color.Yellow; 
+        }
+
+        private void Login_as_admin_MouseLeave(object sender, EventArgs e)
+        {
+            Login_as_admin.ForeColor = Color.White;
+        }
+    }
+}
